c5a1864767c7ad343a4d566697c41290fc6b82ec
BlairCurrey <curreyb88@gmail.com>
2023-03-09 23:59:36 -0500

    fix: wrong current date

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,9 +1,24 @@
-import { dateNowEST } from './'
+import { dateNowEST, formatDate } from './'
 
-describe('dateNowEST', () => {
-  it('should be correct format', () => {
-    const now = dateNowEST();
-    const regEx = /^\d{4}-\d{2}-\d{2}$/;
-    expect(now.match(regEx)).not.toBe(null);
+describe('utils', () => {
+  describe('dateNowEST', () => {
+    it('should be correct format', () => {
+      const now = dateNowEST();
+      const regEx = /^\d{4}-\d{2}-\d{2}$/;
+      expect(now.match(regEx)).not.toBe(null);
+    });
+  });
+  describe('formatDate', () => {
+    it('formats date to YYYY-MM-DD without changing tz', () => {
+      const cases = [
+        {in: '3/9/2023', out: '2023-03-09'},
+        {in: '12/10/2023', out: '2023-12-10'},
+        {in: '3/20/2023', out: '2023-03-20'},
+        {in: '12/9/2023', out: '2023-12-09'}
+      ]
+      cases.forEach((c)=>{
+        expect(formatDate(c.in)).toEqual(c.out)
+      })
+    });
   });
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,14 @@
 export function dateNowEST() {
-  const d = new Date()
-  return d.toISOString().slice(0, 10) // YYYY-MM-DD
+  const now = new Date();
+  const dateString = now.toLocaleDateString('en-US', { timeZone: 'America/New_York' });
+  return formatDate(dateString)
+}
+
+export function formatDate(inputDate: string) {
+  const [month, day, year] = inputDate.split('/');
+  const numericMonth = Number(month);
+  const numericDay = Number(day);
+  const numericYear = Number(year);
+  // Use string interpolation to construct the output string
+  return `${numericYear}-${numericMonth.toString().padStart(2, '0')}-${numericDay.toString().padStart(2, '0')}`;
 }
